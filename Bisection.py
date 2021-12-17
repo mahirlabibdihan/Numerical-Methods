e9589811d251b407db132ce567a058421317df8b
mahirlabibdihan <mahirlabibdihan@gmail.com>
2021-12-17 22:24:43 +0600

    dihu

--- Bisection.py
+++ Bisection.py
@@ -1,25 +1,55 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
 
 
 def f(x):
-    return x*x*x - 0.18*x*x + 0.0004752
+    return x-1
+    # return x*x*x - 0.18*x*x + 0.0004752
+
+# xL>xU -> Invalid
+# xL<xU<root -> Invalid
+# root<xL<sU -> Invalid
+# xL<xU<=root -> return xU
+# root<=xL<xU -> return xL
+# xL<=root<xU -> return xL
+# xL<root<=xU -> return xU
+# xL<=root<=xU -> return xL
+# xL<root<xU -> root(xL, xU, eS, maxIter)
 
 
 def root(xL, xU, eS, maxIter):
+    if xL > xU:
+        print("Error: Lower bound is greater than upper bound.")
+        sys.exit()
+    if f(xL)*f(xU) > 0:
+        print("Error: Function doesn't changes sign on given bounds.")
+        sys.exit()
     xM, xOldM = 0, 0
     for i in range(maxIter):
         xM = (xL+xU)/2
         yL = f(xL)
+        yU = f(xU)
         yM = f(xM)
         if yL*yM < 0:
             xU = xM
-        elif yL*yM > 0:
+        elif yU*yM < 0:
             xL = xM
-        else:
-            return xM
+        elif yL*yM == 0:  # yL=0 or yM=0
+            if yL == 0:
+                return xL
+            else:
+                return xM
+        elif yU*yM == 0:  # yU=0 or yM=0
+            if yU == 0:
+                return xU
+            else:
+                return xM
         if i > 0:
-            eA = abs(xM-xOldM)*100/xM
+            if xM == 0:
+                eA = abs((xM-xOldM)/(xM+sys.float_info.epsilon))*100
+            else:
+                eA = abs((xM-xOldM)/xM)*100
             if eA <= eS:
                 return xM
         xOldM = xM
@@ -27,21 +57,36 @@ def root(xL, xU, eS, maxIter):
 
 
 def table(xL, xU, eS, maxIter):
+    if f(xL)*f(xU) > 0:
+        print("Root can't be calculated")
+        sys.exit()
     print("Table:")
     print("Iteration No\t\tAbsolute Relative Approximate Error")
     xM, xOldM = 0, 0
     for i in range(maxIter):
         xM = (xL+xU)/2
         yL = f(xL)
+        yU = f(xU)
         yM = f(xM)
         if yL*yM < 0:
             xU = xM
-        elif yL*yM > 0:
+        elif yU*yM < 0:
             xL = xM
-        else:
-            return xM
+        elif yL*yM == 0:
+            if yL == 0:
+                return xL
+            else:
+                return xM
+        elif yU*yM == 0:
+            if yU == 0:
+                return xU
+            else:
+                return xM
         if i > 0:
-            eA = abs(xM-xOldM)*100/xM
+            if xM == 0:
+                eA = abs((xM-xOldM)/(xM+sys.float_info.epsilon))*100
+            else:
+                eA = abs((xM-xOldM)/xM)*100
             print('{:12d}\t\t{:.6f}%'.format(i+1, eA))
             if eA <= eS:
                 return xM
@@ -51,9 +96,9 @@ def table(xL, xU, eS, maxIter):
 
 
 def drawGraph():
-    x = np.array(np.arange(0, 0.13, 0.01))
-    y = np.array([f(x[i]) for i in range(13)])
-    plt.plot(x, y, marker='o')
+    x = np.array(np.arange(-10, 10, 1))
+    y = f(x)
+    plt.plot(x, y)
     plt.grid()
     plt.title("f(x)=x^3 - 0.18x^2 + 4.752x10^-4")
     plt.xlabel("x")
@@ -68,7 +113,7 @@ def main():
     maxIter = int(input("Enter max allowed iteration: "))
     print("Depth of the submerged ball is {:.6f}  meter".format(
         root(xL, xU, 0.5, maxIter)))
-    table(0, 0.12, 0.5, 20)
+    table(xL, xU, 0.5, 20)
 
 
 if __name__ == "__main__":
---
+++ Newton-Raphson.py
@@ -0,0 +1,65 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+import sys
+
+
+def f(x):
+    # return math.sin(x)
+    # return x*x*x - 0.165*x*x + 0.0003993
+    # return (x-1)**3+0.512
+    # return x*x+2
+    return -(x-2)**2+2
+
+
+def fd(x):
+    # return math.cos(x)
+    # return 3*x*x - 0.33*x
+    # return 3*(x-1)**2
+    # return 2*x
+
+
+def root(x, eS, maxIter):
+    for i in range(maxIter):
+        if fd(x) == 0:
+            print("Error: Slope of current point is 0.")
+            sys.exit()
+        xNew = x - (f(x)/fd(x))
+        if xNew == 0:
+            eA = abs((xNew-x)/(xNew+sys.float_info.epsilon))*100
+        else:
+            eA = abs((xNew-x)/xNew)*100
+        print(x, xNew, eA)
+        if eA <= eS:
+            # print("Process ended: Relative error is less than", eS)
+            return xNew
+        x = xNew
+    # print("Process ended: Maximum iteration limit exceeded.")
+    return x
+
+
+def drawGraph():
+    x = np.array(np.arange(-1, 1, .01))
+    y = f(x)
+    plt.plot(x, y)
+    plt.grid()
+    plt.title("f(x)=x^3 - 0.165x^2 + 3.993x10^-4")
+    plt.xlabel("x")
+    plt.ylabel("f(x)")
+    plt.show()
+
+
+def main():
+    drawGraph()
+    x = float(input("Estimate root: "))
+    maxIter = int(input("Enter max allowed iteration: "))
+    print(root(x, 0.05, maxIter))
+
+
+if __name__ == "__main__":
+    main()
+# x^3 - 0.165x^2 + 3.993x10^-4
+# 0.14636
+# 0.06238
+# -0.04374
+# tangent in 0 at x=0,0.11
